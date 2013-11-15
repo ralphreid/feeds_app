329c4be2d79c46020d95f0acf694e71a2001d27e
Ralph Reid <beresfordjunior@me.com>
2013-11-15 09:37:03 +0000

    created rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedxApp::Application.config.secret_token = '0592c5efef7874d380cb33e47ee18d55842b021d89ec2a7f0111a807c413920c393345a1c1c92a2b59bc7a6c3cc7f187a32b9bd9d972d64914af92d3fdf676d8'
