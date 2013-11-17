e004667e684a62299e301eb113b2fee87246e3b4
maloneyl <m@thinkmaloney.com>
2013-11-17 18:23:51 +0000

    updated file to include associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,8 @@ class Article < ActiveRecord::Base
 
   # not sure what/if any validation is required if we're pulling all these fields' data from RSS
 
+  belongs_to :feed
+  has_many :tags
+  has_many :users, :through => :tags
+
 end
