b62e87a260f652cbcae14f5aaab01741ee5bcbd4
forbesr <forbesr@tcd.ie>
2013-11-18 16:08:24 +0000

    updated articleand feed model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
 
-  attr_accessible :title, :content, :link, :description, :author, :comment_link
+  attr_accessible :title, :content, :link, :description, :author, :comment_link, :posted_at, :feed_id
+
+  validates_uniqueness_of :link
 
   # not sure what/if any validation is required if we're pulling all these fields' data from RSS
 
@@ -8,4 +10,25 @@ class Article < ActiveRecord::Base
   has_many :tags
   has_many :users, :through => :tags
 
+  def self.get_articles_from_feed(feed)
+    from_feed = Feedzirra::Feed.fetch_and_parse(feed.rss)
+    add_articles(from_feed.entries, feed.id)
+  end
+
+
+  def self.add_articles(articles, feed_id)
+    articles.each do |article|
+      Article.create!(
+          :title         => article.title,
+          :description      => article.summary,
+          :content   => article.content,
+          :link          => article.url,
+          :posted_at => article.published,
+          :author    => article.author,
+          :feed_id   => feed_id
+        )
+    end
+  end
+
 end
+
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,7 +2,7 @@ class Feed < ActiveRecord::Base
 
   attr_accessible :title, :link, :rss, :provider, :category, :description, :image_link, :image_title, :guid, :last_modified
 
-  validates_uniqueness_of :guid
+  validates_uniqueness_of :rss
 
   # not sure what/if any validation is required if we're pulling all these fields' data from RSS
 
