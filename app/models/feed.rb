bc58e24d58d00dcf563ac396d06ef6ba9c1b05ee
Ralph Reid <beresfordjunior@me.com>
2013-11-19 21:26:49 +0000

    updated feed.rb

---
+++ app/models/feed.rb
@@ -0,0 +1,34 @@
+class Feed < ActiveRecord::Base
+
+  attr_accessible :title, :link, :rss, :provider, :category, :description, :image_link, :image_title, :guid, :last_modified
+
+  validates_uniqueness_of :rss
+
+  # not sure what/if any validation is required if we're pulling all these fields' data from RSS
+
+  has_many :articles
+  has_many :subscriptions
+  has_many :users, :through => :subscriptions
+
+  def self.get_feed(rss, category)
+    feed = Feedzirra::Feed.fetch_and_parse(rss)
+    add_feed(feed, category)
+  end
+
+  def self.add_feed(feed, category=nil)
+    f = Feed.where(:rss => feed.feed_url).first_or_create
+    f.title = feed.title
+    f.rss = feed.feed_url
+    f.link = feed.url
+    f.guid = feed.etag
+    f.last_modified = feed.last_modified
+    f.category = category
+    f.save!
+  end
+
+  def self.check_for_update_feed(feed)
+    feed = Feedzirra::Feed.fetch_and_parse(feed.rss)
+    updated_feed = Feedzirra::Feed.update(feed)
+  end
+
+end
