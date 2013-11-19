e473632476a4c3ec97230f66df808f695d66efd5
Ralph Reid <beresfordjunior@me.com>
2013-11-19 23:13:51 +0000

    added user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < Devise::RegistrationsController
   end
 
   def show
-    
+    @user = User.find params[:id]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 FeedxApp::Application.routes.draw do
-  devise_for :users, :controllers => {:registrations => 'users'}, :path_prefix => 'my'
+  devise_for :users, :controllers => {:registrations => 'users'}
+  # , :path_prefix => 'my'
   devise_scope :user do
-    get "/users/show" => "users#show"
+    # get "/users/show/:id" => "users#show"
+    resources :users, :only => [:show] 
   end
+
   resources :feeds do
     resources :articles
     post 'subscribe', :on => :member
